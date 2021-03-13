ff3b2dc2e66a93723ef45b9a2e686d70440ec21d
Abhineet Tamrakar <abhineet97@gmail.com>
2021-03-13 23:53:47 +0530

    Copy more assests to Eleventy output

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("manifest.webmanifest");
+  eleventyConfig.addPassthroughCopy("sw.js");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
 };
