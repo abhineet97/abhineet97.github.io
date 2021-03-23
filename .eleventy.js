b14c6f7c65c540c3dda32cdffcd477eba963b2d6
Abhineet Tamrakar <abhineet97@gmail.com>
2021-03-23 20:51:36 +0530

    Move CSS to separate file for convenient editing

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,5 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("manifest.webmanifest");
   eleventyConfig.addPassthroughCopy("sw.js");
   eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("styles.css");
 };
